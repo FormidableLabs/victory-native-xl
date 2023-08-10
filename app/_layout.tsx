6dd4952416f4c500941f8b0b9b4dae0870778e80
Carlos Kelly <me@carlos.dev>
2023-08-10 07:57:00 -0500

    Add app navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import * as React from "react";
 import { SafeAreaView } from "react-native";
 
 export default function Layout() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Slot />
+      <Stack />
     </SafeAreaView>
   );
 }
