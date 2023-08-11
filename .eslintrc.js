0b168716c29c015bb9ff6db5464219bf4ec435c6
Carlos Kelly <me@carlos.dev>
2023-08-11 06:54:26 -0500

    Add ESLint and TypeCheck

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react", "prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "react/react-in-jsx-scope": 0,
+  },
+};
