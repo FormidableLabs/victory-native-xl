2d6fd867cf6a4fb4eb9a361e218689f18e8d6b55
Carlos Kelly <me@carlos.dev>
2023-08-20 21:18:13 -0500

    Create Options Sheet (#25)

--- example/app/_layout.tsx
+++ example/app/_layout.tsx
@@ -1,22 +1,11 @@
-import { ChartOptionsProvider } from "example/components/OptionsProvider";
 import { Stack } from "expo-router";
 import * as React from "react";
 import { SafeAreaView } from "react-native";
 
 export default function Layout() {
   return (
-    <ChartOptionsProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Stack>
-          <Stack.Screen
-            name="chart-options-modal"
-            options={{
-              title: "Chart Options",
-              presentation: "modal",
-            }}
-          />
-        </Stack>
-      </SafeAreaView>
-    </ChartOptionsProvider>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack />
+    </SafeAreaView>
   );
 }
---
+++ example/components/InputSlider.tsx
@@ -0,0 +1,42 @@
+import Slider from "@react-native-community/slider";
+import * as React from "react";
+import { Text, View } from "react-native";
+
+type Props = {
+  label: string;
+  value: number;
+  minValue: number;
+  maxValue: number;
+  step: number;
+  onChange(value: number): void;
+};
+
+export const InputSlider = ({
+  label,
+  maxValue,
+  minValue,
+  step,
+  value,
+  onChange,
+}: Props) => (
+  <View style={{ marginVertical: 10 }}>
+    <Text style={{ fontWeight: "bold" }}>{label}</Text>
+    <View
+      style={{
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <Slider
+        style={{ width: "90%", height: 40 }}
+        minimumValue={minValue}
+        maximumValue={maxValue}
+        step={step}
+        value={value}
+        onValueChange={onChange}
+      />
+      <Text style={{ alignSelf: "center" }}>{value}</Text>
+    </View>
+  </View>
+);
