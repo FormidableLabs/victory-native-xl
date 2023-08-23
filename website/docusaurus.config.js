b9ee2258d10a89c57194dda9f2ccb8b88e14b408
Carlos Kelly <me@carlos.dev>
2023-08-22 19:16:29 -0500

    Base 🦖 set up.

---
+++ website/docusaurus.config.js
@@ -0,0 +1,86 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Victory Native",
+  tagline:
+    "A charting library for React Native with a focus on performance and customization.",
+  url: "https://formidable.com",
+  baseUrl:
+    // eslint-disable-next-line no-undef
+    process.env.VERCEL_ENV === "preview" ? "/" : "/open-source/victory-native",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  organizationName: "formidablelabs",
+  projectName: "victory-native",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/",
+          path: "../docs",
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl:
+            "https://github.com/FormidableLabs/victory-native-xl/tree/master/website",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "VICTORY NATIVE XL",
+        logo: {
+          alt: "Victory Native",
+          src: "img/victory-icon.svg",
+        },
+        items: [
+          {
+            href: "https://github.com/FormidableLabs/victory-native-xl",
+            className: "header-github-link",
+            "aria-label": "GitHub Repository",
+            position: "right",
+            label: "GitHub Repository",
+          },
+          {
+            href: "https://formidable.com",
+            className: "header-formidable-link",
+            "aria-label": "Formidable Website",
+            position: "right",
+            label: "Formidable Website",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        copyright: `Copyright © ${new Date().getFullYear()} Formidable`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
