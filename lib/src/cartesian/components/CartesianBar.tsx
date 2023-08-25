8c58f93b9ada2a2ab26a8ab755330ce93fbdd8c7
Carlos Kelly <me@carlos.dev>
2023-08-25 15:36:15 -0500

    Update CartesianBar.tsx

--- lib/src/cartesian/components/CartesianBar.tsx
+++ lib/src/cartesian/components/CartesianBar.tsx
@@ -24,28 +24,29 @@ export const CartesianBar = ({
 }: CartesianBarProps) => {
   const [y0 = 0, y1 = 0] = yScale.domain();
 
-  const scale = React.useMemo(() => {
-    return scaleBand()
+  const path = React.useMemo(() => {
+    const path = Skia.Path.Make();
+    const scale = scaleBand()
       .range([xScale(points.at(0)!.xValue!), xScale(points.at(-1)!.xValue!)])
       .domain(points.map((p) => p.xValue.toFixed(0)))
-      .paddingInner(0.25)
+      .paddingInner(0.5)
       .paddingOuter(-0.5)
       .align(0.5)
       .round(false);
+
+    points.forEach((point) => {
+      path.addRect(
+        Skia.XYWHRect(
+          scale(point.xValue.toFixed(0))!,
+          point.y!,
+          scale.bandwidth(),
+          -(yScale(y0) - yScale(y1) - point.y),
+        ),
+      );
+    });
+
+    return path;
   }, [xScale, yScale, points]);
-
-  const path = Skia.Path.Make();
-
-  points.forEach((point) => {
-    path.addRect(
-      Skia.XYWHRect(
-        scale(point.xValue.toFixed(0))!,
-        point.y!,
-        scale.bandwidth(),
-        -(yScale(y0) - yScale(y1) - point.y),
-      ),
-    );
-  });
 
   return React.createElement(isAnimated ? AnimatedPath : Path, {
     path,
