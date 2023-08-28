0e0778ee17b8477dcd106c9eeda8bfc4db0d8ad8
Grant Sander <gksander93@gmail.com>
2023-08-28 10:15:36 -0500

    Expose ChartPressValue type

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -16,7 +16,10 @@ export { type CurveType } from "./cartesian/utils/curves";
 
 export { useAnimatedPath } from "./hooks/useAnimatedPath";
 export { usePrevious } from "./utils/usePrevious";
-export { useChartPressSharedValue } from "./cartesian/hooks/useChartPressSharedValue";
+export {
+  useChartPressSharedValue,
+  type ChartPressValue,
+} from "./cartesian/hooks/useChartPressSharedValue";
 
 // Line
 export { useLinePath } from "./cartesian/hooks/useLinePath";
