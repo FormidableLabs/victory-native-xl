dbed37439a791af7cafb270a4524423650fa9646
Carlos Kelly <me@carlos.dev>
2023-08-29 15:34:04 -0500

    Remove typecheck from JS files.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,4 +1,3 @@
-// @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require("prism-react-renderer/themes/github");
--- website/sidebars.js
+++ website/sidebars.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   docsSidebar: [
