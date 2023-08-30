9a84ea30cd0d82ee7a7464a4442bb98f2da872e7
Grant Sander <gksander93@gmail.com>
2023-08-30 08:45:25 -0500

    rm unused type

--- lib/src/types.ts
+++ lib/src/types.ts
@@ -106,15 +106,3 @@ export type AxisProps<
   formatXLabel: (label: T[XK]) => string;
   formatYLabel: (label: T[YK]) => string;
 };
-
-/**
- * Utility types
- */
-export type Tuple<T, N extends number> = N extends N
-  ? number extends N
-    ? T[]
-    : _TupleOf<T, N, []>
-  : never;
-type _TupleOf<T, N extends number, R extends unknown[]> = R["length"] extends N
-  ? R
-  : _TupleOf<T, N, [T, ...R]>;
