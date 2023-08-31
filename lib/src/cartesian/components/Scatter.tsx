9abbc0da6abbf4b43e509cea884b16409df46c65
Keith Luchtel <keith@keithluchtel.com>
2023-08-31 13:57:14 -0500

    Update Scatter props (#84)
    Updates Scatter props to accept 'start' and 'end' as well
    as some animated values.

--- lib/src/cartesian/components/Scatter.tsx
+++ lib/src/cartesian/components/Scatter.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
-import { Path, Skia } from "@shopify/react-native-skia";
+import {
+  Path,
+  Skia,
+  type PathProps,
+  type SkiaDefaultProps,
+} from "@shopify/react-native-skia";
 import type { PointsArray } from "../../types";
 import { AnimatedPath } from "./AnimatedPath";
 import type { PathAnimationConfig } from "../../hooks/useAnimatedPath";
@@ -7,13 +12,20 @@ import type { PathAnimationConfig } from "../../hooks/useAnimatedPath";
 type ScatterProps = {
   points: PointsArray;
   animate?: PathAnimationConfig;
-  color?: string;
-};
+} & SkiaDefaultProps<
+  Pick<PathProps, "color" | "start" | "end">,
+  "start" | "end"
+>;
 
 /**
  * TODO: Colors, sizes, shapes
  */
-export function Scatter({ points, animate, color = "black" }: ScatterProps) {
+export function Scatter({
+  points,
+  animate,
+  color = "black",
+  ...rest
+}: ScatterProps) {
   const path = React.useMemo(() => {
     const p = Skia.Path.Make();
 
@@ -28,6 +40,7 @@ export function Scatter({ points, animate, color = "black" }: ScatterProps) {
     path,
     style: "fill",
     color,
+    ...rest,
     ...(Boolean(animate) && { animate }),
   });
 }
