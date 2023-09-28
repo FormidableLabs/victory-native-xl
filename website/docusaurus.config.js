d6f3899a283c20444791c2f6a0c06edd7c47490f
Grant Sander <gksander93@gmail.com>
2023-09-28 13:56:42 -0500

    Docusaurus banner

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -62,6 +62,13 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      announcementBar: {
+        id: "supportus",
+        content: `These docs are for version 40 of Victory Native. If you're looking for docs on versions <=36, please <a href="https://formidable.com/open-source/victory/docs/native" rel="noreferrer" target="_blank">see here</a>.`,
+        textColor: "var(--banner-text)",
+        backgroundColor: "var(--banner-bg)",
+      },
+
       navbar: {
         title: "VICTORY NATIVE XL",
         logo: {
