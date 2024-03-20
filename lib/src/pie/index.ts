445e1321779d519f83b12d19c5f8a3e77d16c7dc
Eli Zibin <elizibin@gmail.com>
2024-03-20 14:30:54 -0700

    fix build error
    we don't really need to expose this + it's undocumented and less useful to users than it is internally.

--- lib/src/pie/index.ts
+++ lib/src/pie/index.ts
@@ -1,12 +1,10 @@
 import { PieChart } from "./PieChart";
 import { PieSlice } from "./PieSlice";
 import { PieSliceAngularInset } from "./PieSliceAngularInset";
-import { PieSliceProvider } from "./contexts/PieSliceContext";
 
 const Pie = {
   Chart: PieChart,
   Slice: PieSlice,
-  SliceProvider: PieSliceProvider,
   SliceAngularInset: PieSliceAngularInset,
 };
 
