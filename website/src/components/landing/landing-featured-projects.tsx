58e9e11c8e25d9e624e7c2de47c6430cc6b72e1b
Christy Presler <cpresler@users.noreply.github.com>
2024-05-20 14:24:10 -0400

    footer and featured project layout adjustments

--- website/src/components/landing/landing-featured-projects.tsx
+++ website/src/components/landing/landing-featured-projects.tsx
@@ -28,28 +28,28 @@ export const LandingFeaturedProjects = ({
   }[];
   showDivider?: boolean;
 }) => (
-  <div className="flex flex-col text-left mx-16 lg:mx-32 xl:mx-64 mt-12 py-12">
+  <div className="flex flex-col gap-6 text-center md:text-left mx-16 lg:mx-32 xl:mx-64 mt-12 py-12">
     {showDivider && <Divider />}
     <h2 className="my-8 text-4xl font-semibold">{heading}</h2>
-    <div className="grid grid-cols-2 gap-8">
+    <div className="grid grid-cols-2 gap-12">
       {projects.map(({ name, link, description, title }) => (
         <a
           href={link}
           key={link}
-          className="col-span-2 sm:col-span-1 block grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 align-center items-center text-theme-2 hover:text-theme-2 dark:text-white dark:hover:text-white"
+          className="col-span-2 sm:col-span-1 flex flex-col lg:flex-row gap-6 align-center items-center text-theme-2 hover:text-theme-2 dark:text-white dark:hover:text-white"
         >
-          <FeaturedBadge name={name} isHoverable className="col-span-1" />
-          <span className="flex flex-col col-span-1 lg:col-span-2">
+          <FeaturedBadge name={name} isHoverable className="lg:basis-1/3 max-w-xs md:justify-self-end" />
+          <span className="flex flex-col lg:basis-2/3 text-center md:text-left">
             <span className="text-xl font-semibold capitalize">
               {title || name}
             </span>
-            <span className="text-sm ">{description}</span>
+            <span className="text-sm max-w-md">{description}</span>
           </span>
         </a>
       ))}
     </div>
 
-    <div className="my-8 pt-8 align-center">
+    <div className="flex my-8 pt-8 justify-center md:justify-start">
       <NFLinkButton
         link="https://commerce.nearform.com/open-source"
         text="View All Projects"
