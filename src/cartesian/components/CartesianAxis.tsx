a57a464980668e1bd174ee30fad7c2d636a00f19
KyleMDavidson <ksonofdavid@gmail.com>
2024-09-13 12:21:55 -0400

    some logs to check things out. Turns out we cannot fit our x label content, and that's why it's not being rendered.

---
+++ src/cartesian/components/CartesianAxis.tsx
@@ -0,0 +1,286 @@
+import React, { useMemo } from "react";
+import {
+  Line,
+  Path,
+  Skia,
+  Text,
+  vec,
+  type Color,
+} from "@shopify/react-native-skia";
+import { StyleSheet } from "react-native";
+import type {
+  ValueOf,
+  NumericalFields,
+  InputDatum,
+  AxisProps,
+  InputFields,
+} from "../../types";
+import { DEFAULT_TICK_COUNT } from "../../utils/tickHelpers";
+
+export const CartesianAxis = <
+  RawData extends Record<string, unknown>,
+  XK extends keyof InputFields<RawData>,
+  YK extends keyof NumericalFields<RawData>,
+>({
+  tickCount = DEFAULT_TICK_COUNT,
+  xTicksNormalized,
+  yTicksNormalized,
+  labelPosition = "outset",
+  labelOffset = { x: 2, y: 4 },
+  axisSide = { x: "bottom", y: "left" },
+  lineColor = "hsla(0, 0%, 0%, 0.25)",
+  lineWidth = StyleSheet.hairlineWidth,
+  labelColor = "#000000",
+  formatYLabel = (label: ValueOf<InputDatum>) => String(label),
+  formatXLabel = (label: ValueOf<InputDatum>) => String(label),
+  yScale,
+  xScale,
+  font,
+  isNumericalData = false,
+  ix = [],
+}: AxisProps<RawData, XK, YK>) => {
+  const axisConfiguration = useMemo(() => {
+    return {
+      xTicks: typeof tickCount === "number" ? tickCount : tickCount.x,
+      yTicks: typeof tickCount === "number" ? tickCount : tickCount.y,
+      xLabelOffset:
+        typeof labelOffset === "number" ? labelOffset : labelOffset.x,
+      yLabelOffset:
+        typeof labelOffset === "number" ? labelOffset : labelOffset.y,
+      xAxisPosition: axisSide.x,
+      yAxisPosition: axisSide.y,
+      xLabelPosition:
+        typeof labelPosition === "string" ? labelPosition : labelPosition.x,
+      yLabelPosition:
+        typeof labelPosition === "string" ? labelPosition : labelPosition.y,
+      gridXLineColor: (typeof lineColor === "object" && "grid" in lineColor
+        ? typeof lineColor.grid === "object" && "x" in lineColor.grid
+          ? lineColor.grid.x
+          : lineColor.grid
+        : lineColor) as Color,
+      gridYLineColor: (typeof lineColor === "object" && "grid" in lineColor
+        ? typeof lineColor.grid === "object" && "y" in lineColor.grid
+          ? lineColor.grid.y
+          : lineColor.grid
+        : lineColor) as Color,
+      gridFrameLineColor: (typeof lineColor === "object" && "frame" in lineColor
+        ? lineColor.frame
+        : lineColor) as Color,
+      gridXLineWidth:
+        typeof lineWidth === "object" && "grid" in lineWidth
+          ? typeof lineWidth.grid === "object" && "x" in lineWidth.grid
+            ? lineWidth.grid.x
+            : lineWidth.grid
+          : lineWidth,
+      gridYLineWidth:
+        typeof lineWidth === "object" && "grid" in lineWidth
+          ? typeof lineWidth.grid === "object" && "y" in lineWidth.grid
+            ? lineWidth.grid.y
+            : lineWidth.grid
+          : lineWidth,
+      gridFrameLineWidth:
+        typeof lineWidth === "object" && "frame" in lineWidth
+          ? lineWidth.frame
+          : lineWidth,
+    } as const;
+  }, [
+    tickCount,
+    labelOffset,
+    axisSide.x,
+    axisSide.y,
+    labelPosition,
+    lineColor,
+    lineWidth,
+  ]);
+
+  const {
+    xTicks,
+    yTicks,
+    xAxisPosition,
+    yAxisPosition,
+    xLabelPosition,
+    yLabelPosition,
+    xLabelOffset,
+    yLabelOffset,
+    gridXLineColor,
+    gridYLineColor,
+    gridFrameLineColor,
+    gridXLineWidth,
+    gridYLineWidth,
+    gridFrameLineWidth,
+  } = axisConfiguration;
+
+  const [x1 = 0, x2 = 0] = xScale.domain();
+  const [y1 = 0, y2 = 0] = yScale.domain();
+  const [x1r = 0, x2r = 0] = xScale.range();
+  const fontSize = font?.getSize() ?? 0;
+
+  const yAxisNodes = yTicksNormalized.map((tick) => {
+    const contentY = formatYLabel(tick as never);
+    const labelWidth =
+      font
+        ?.getGlyphWidths?.(font.getGlyphIDs(contentY))
+        .reduce((sum, value) => sum + value, 0) ?? 0;
+    const labelY = yScale(tick) + fontSize / 3;
+    const labelX = (() => {
+      // left, outset
+      if (yAxisPosition === "left" && yLabelPosition === "outset") {
+        return xScale(x1) - (labelWidth + yLabelOffset);
+      }
+      // left, inset
+      if (yAxisPosition === "left" && yLabelPosition === "inset") {
+        return xScale(x1) + yLabelOffset;
+      }
+      // right, outset
+      if (yAxisPosition === "right" && yLabelPosition === "outset") {
+        return xScale(x2) + yLabelOffset;
+      }
+      // right, inset
+      return xScale(x2) - (labelWidth + yLabelOffset);
+    })();
+
+    const canFitLabelContent = labelY > fontSize && labelY < yScale(y2);
+
+    return (
+      <React.Fragment key={`y-tick-${tick}`}>
+        <Line
+          p1={vec(xScale(x1), yScale(tick))}
+          p2={vec(xScale(x2), yScale(tick))}
+          color={gridYLineColor}
+          strokeWidth={gridYLineWidth}
+        />
+        {font
+          ? canFitLabelContent && (
+              <Text
+                color={
+                  typeof labelColor === "string" ? labelColor : labelColor.y
+                }
+                text={contentY}
+                font={font}
+                y={labelY}
+                x={labelX}
+              />
+            )
+          : null}
+      </React.Fragment>
+    );
+  });
+
+  const xAxisNodes = xTicksNormalized.map((tick) => {
+    //    const val = isNumericalData ? tick : ix[tick];
+    //    KD here just forcing this for now.
+    const val = tick;
+    console.log(`building an xNode in cartesian axis with val`);
+    console.log(val);
+    const contentX = formatXLabel(val as never);
+    console.log(`content of node with formatted text value:`);
+    console.log(contentX);
+    console.log(`typeof node text value: ${typeof contentX} `);
+    const labelWidth =
+      font
+        ?.getGlyphWidths?.(font.getGlyphIDs(contentX))
+        .reduce((sum, value) => sum + value, 0) ?? 0;
+
+    console.log(`labelwidth in axis : ${labelWidth}`);
+    const labelX = xScale(tick) - (labelWidth ?? 0) / 2;
+    //even if we force this to be true, no labels perceptible.
+    const canFitLabelContent =
+      yAxisPosition === "left" ? labelX + labelWidth < x2r : x1r < labelX;
+
+    console.log(`can we fit label content? ${canFitLabelContent}`);
+    console.log(`do we have our font? ${font != null}`);
+    console.log(`do we have a label weidth calculated? ${labelWidth}`);
+
+    const labelY = (() => {
+      // bottom, outset
+      if (xAxisPosition === "bottom" && xLabelPosition === "outset") {
+        return yScale(y2) + xLabelOffset + fontSize;
+      }
+      // bottom, inset
+      if (xAxisPosition === "bottom" && xLabelPosition === "inset") {
+        return yScale(y2) - xLabelOffset;
+      }
+      // top, outset
+      if (xAxisPosition === "top" && xLabelPosition === "outset") {
+        return yScale(y1) - xLabelOffset;
+      }
+      // top, inset
+      return yScale(y1) + fontSize + xLabelOffset;
+    })();
+
+    return (
+      <React.Fragment key={`x-tick-${tick}`}>
+        <Line
+          p1={vec(xScale(tick), yScale(y2))}
+          p2={vec(xScale(tick), yScale(y1))}
+          color={gridXLineColor}
+          strokeWidth={gridXLineWidth}
+        />
+        {font && labelWidth && canFitLabelContent ? (
+          <Text
+            color={typeof labelColor === "string" ? labelColor : labelColor.x}
+            text={contentX}
+            font={font}
+            y={labelY}
+            x={labelX}
+          />
+        ) : null}
+      </React.Fragment>
+    );
+  });
+
+  const boundingFrame = React.useMemo(() => {
+    const framePath = Skia.Path.Make();
+
+    framePath.addRect(
+      Skia.XYWHRect(
+        xScale(x1),
+        yScale(y1),
+        xScale(x2) - xScale(x1),
+        yScale(y2) - yScale(y1),
+      ),
+    );
+    return framePath;
+  }, [x1, x2, xScale, y1, y2, yScale]);
+
+  //we still get a cyclic structure in JSON object error when passing this to JSON.stingify's 2nd parameter.
+  //no, actually, we fucking get cannot read property includes of null
+  function cyclic_stringify_helper(key, val) {
+    let seen = [];
+    if (val != null && typeof val == "object") {
+      if (seen.indexOf(val) >= 0) {
+        return;
+      }
+      seen.push(val);
+    }
+    return val;
+  }
+
+  console.log("xticks > 0:");
+  console.log(xTicks > 0);
+  return (
+    <>
+      {xTicks > 0 ? xAxisNodes : null}
+      {yTicks > 0 ? yAxisNodes : null}
+      <Path
+        path={boundingFrame}
+        strokeWidth={gridFrameLineWidth}
+        style="stroke"
+        color={gridFrameLineColor}
+      />
+    </>
+  );
+};
+
+export const CartesianAxisDefaultProps = {
+  lineColor: "hsla(0, 0%, 0%, 0.25)",
+  lineWidth: StyleSheet.hairlineWidth,
+  tickCount: 5,
+  labelOffset: { x: 2, y: 4 },
+  axisSide: { x: "bottom", y: "left" },
+  labelPosition: "outset",
+  formatXLabel: (label: ValueOf<InputDatum>) => String(label),
+  formatYLabel: (label: ValueOf<InputDatum>) => String(label),
+  labelColor: "#000000",
+  ix: [],
+} satisfies Partial<AxisProps<never, never, never>>;
