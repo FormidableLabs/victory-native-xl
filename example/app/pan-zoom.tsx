44653f4e6df9141362e979a086427d2c1b22b8db
Keith Luchtel <keith@keithluchtel.com>
2024-10-30 14:34:37 -0500

    Add more actions and setup example

--- example/app/pan-zoom.tsx
+++ example/app/pan-zoom.tsx
@@ -1,16 +1,62 @@
-import { Button } from "../components/Button";
+import {
+  useAnimatedReaction,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 export const PanZoom = () => {};
 import * as React from "react";
 import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
-import { CartesianChart, Line, useChartTransformState } from "victory-native";
+import {
+  CartesianChart,
+  type ChartBounds,
+  Line,
+  useChartTransformState,
+} from "victory-native";
 import { useFont } from "@shopify/react-native-skia";
+import { useState } from "react";
 import { appColors } from "./consts/colors";
 import inter from "../assets/inter-medium.ttf";
 
+import { Button } from "../components/Button";
+
 export default function PanZoomPage() {
   const font = useFont(inter, 12);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
   const { state, actions } = useChartTransformState();
+
+  const k = useSharedValue(1);
+  const tx = useSharedValue(0);
+  const ty = useSharedValue(0);
+
+  useAnimatedReaction(
+    () => {
+      return state.panActive.value || state.zoomActive.value;
+    },
+    (cv, pv) => {
+      if (!cv && pv) {
+        const vals = actions.getTransformComponents();
+        k.value = vals.scaleX;
+        tx.value = vals.translateX;
+        ty.value = vals.translateY;
+
+        k.value = withTiming(1);
+        tx.value = withTiming(0);
+        ty.value = withTiming(0);
+      }
+    },
+  );
+
+  useAnimatedReaction(
+    () => {
+      return { k: k.value, tx: tx.value, ty: ty.value };
+    },
+    ({ k, tx, ty }) => {
+      actions.setTranslate(tx, ty);
+      actions.setScale(k);
+    },
+  );
 
   return (
     <SafeAreaView style={styles.safeView}>
@@ -23,6 +69,10 @@ export default function PanZoomPage() {
             font,
           }}
           transformState={state}
+          onChartBoundsChange={({ top, left, right, bottom }) => {
+            setWidth(right - left);
+            setHeight(bottom - top);
+          }}
         >
           {({ points }) => (
             <>
@@ -38,22 +88,58 @@ export default function PanZoomPage() {
       >
         <View style={{ gap: 10 }}>
           <View style={{ flexDirection: "row", gap: 20 }}>
-            <Button title={"Pan Left"} style={{ flex: 1 }} />
-            <Button title={"Pan Right"} style={{ flex: 1 }} />
+            <Button
+              title={"Pan Left"}
+              style={{ flex: 1 }}
+              onPress={() => {
+                actions.translate(10, 0);
+              }}
+            />
+            <Button
+              title={"Pan Right"}
+              style={{ flex: 1 }}
+              onPress={() => {
+                actions.translate(-10, 0);
+              }}
+            />
           </View>
           <View style={{ flexDirection: "row", gap: 20 }}>
-            <Button title={"Pan Up"} style={{ flex: 1 }} />
-            <Button title={"Pan Down"} style={{ flex: 1 }} />
+            <Button
+              title={"Pan Up"}
+              style={{ flex: 1 }}
+              onPress={() => {
+                actions.translate(0, 10);
+              }}
+            />
+            <Button
+              title={"Pan Down"}
+              style={{ flex: 1 }}
+              onPress={() => {
+                actions.translate(0, -10);
+              }}
+            />
           </View>
           <View style={{ flexDirection: "row", gap: 20 }}>
             <Button
               title={"Zoom In"}
               style={{ flex: 1 }}
               onPress={() => {
-                actions.setScale();
+                actions.scale(1.5, 1.5, {
+                  x: width / 2,
+                  y: height / 2,
+                });
               }}
             />
-            <Button title={"Zoom Out"} style={{ flex: 1 }} />
+            <Button
+              title={"Zoom Out"}
+              style={{ flex: 1 }}
+              onPress={() => {
+                actions.scale(0.5, 0.5, {
+                  x: width / 2,
+                  y: height / 2,
+                });
+              }}
+            />
           </View>
         </View>
       </ScrollView>
--- lib/src/cartesian/hooks/useChartTransformState.ts
+++ lib/src/cartesian/hooks/useChartTransformState.ts
@@ -3,7 +3,12 @@ import {
   type SharedValue,
   useSharedValue,
 } from "react-native-reanimated";
-import { type Matrix4 } from "@shopify/react-native-skia";
+import {
+  type Matrix4,
+  multiply4,
+  scale,
+  translate,
+} from "@shopify/react-native-skia";
 
 export const identity4: Matrix4 = [
   1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1,
@@ -16,6 +21,17 @@ enum MatrixValues {
   TranslateY = 7,
 }
 const getActions = (matrix: SharedValue<Matrix4>) => {
+  const getTransformComponents = () => {
+    "worklet";
+
+    return {
+      scaleX: matrix.value[MatrixValues.ScaleX],
+      scaleY: matrix.value[MatrixValues.ScaleY],
+      translateX: matrix.value[MatrixValues.TranslateX],
+      translateY: matrix.value[MatrixValues.TranslateY],
+    };
+  };
+
   const setScale = (kx: number, ky?: number) => {
     "worklet";
     const next = matrix.value.slice(0);
@@ -35,15 +51,38 @@ const getActions = (matrix: SharedValue<Matrix4>) => {
     matrix.value = next as unknown as Matrix4;
   };
 
+  const _translate = (tx: number, ty: number) => {
+    "worklet";
+
+    matrix.value = multiply4(matrix.value, translate(tx, ty));
+  };
+
+  const _scale = (kx: number, ky?: number, p?: { x: number; y: number }) => {
+    "worklet";
+
+    matrix.value = multiply4(matrix.value, scale(kx, ky ?? kx, 1, p));
+  };
+
   return {
+    getTransformComponents: getTransformComponents,
     setScale: setScale,
     setTranslate: setTranslate,
+    translate: _translate,
+    scale: _scale,
   };
 };
 
 type CharTransformActions = {
   setScale: (kx: number, ky?: number) => void;
   setTranslate: (tx: number, ty: number) => void;
+  getTransformComponents: () => {
+    scaleX: number;
+    scaleY: number;
+    translateX: number;
+    translateY: number;
+  };
+  translate: (tx: number, ty: number) => void;
+  scale: (kx: number, ky?: number, p?: { x: number; y: number }) => void;
 };
 
 export type ChartTransformState = {
