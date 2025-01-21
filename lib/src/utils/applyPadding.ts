f489273efc00f0a68dd30f043f49efcd68fa80b5
Dan McGrath <daniel.mcgrath9@gmail.com>
2025-01-21 15:42:34 -0500

    refactor: use sided number utility in applyPadding

--- lib/src/utils/applyPadding.ts
+++ lib/src/utils/applyPadding.ts
@@ -1,21 +1,30 @@
 import type { ChartBounds, SidedNumber } from "../types";
+import { valueFromSidedNumber } from "./valueFromSidedNumber";
 
 export default function applyPadding(
   bounds: ChartBounds,
   padding: SidedNumber,
 ) {
-  const paddedBounds = {
+  return {
     left:
       bounds.left -
-      (typeof padding === "number" ? padding : padding?.left ?? 0),
+      (typeof padding === "number"
+        ? padding
+        : valueFromSidedNumber(padding, "left")),
     right:
       bounds.right +
-      (typeof padding === "number" ? padding : padding?.right ?? 0),
+      (typeof padding === "number"
+        ? padding
+        : valueFromSidedNumber(padding, "right")),
     top:
-      bounds.top + (typeof padding === "number" ? padding : padding?.top ?? 0),
+      bounds.top +
+      (typeof padding === "number"
+        ? padding
+        : valueFromSidedNumber(padding, "top")),
     bottom:
       bounds.bottom -
-      (typeof padding === "number" ? padding : padding?.bottom ?? 0),
+      (typeof padding === "number"
+        ? padding
+        : valueFromSidedNumber(padding, "bottom")),
   };
-  return paddedBounds;
 }
