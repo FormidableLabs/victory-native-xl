f30f85f79dc77fadf23346998f7c465eda375f4c
iuliu Vișovan <iuliuvisovan@gmail.com>
2025-10-04 00:26:20 +0300

    Undo gtting-started

--- example/app/guides/getting-started.tsx
+++ example/app/guides/getting-started.tsx
@@ -1,52 +1,48 @@
 import * as React from "react";
-import { View } from "react-native";
-import {
-  CartesianChart,
-  Line,
-  useChartPressState,
-  useChartTransformState,
-} from "victory-native";
+import { StyleSheet, View, SafeAreaView, Linking } from "react-native";
+import { CartesianChart, Line, useChartPressState } from "victory-native";
 import { Circle, useFont } from "@shopify/react-native-skia";
 import type { SharedValue } from "react-native-reanimated";
+import { Button } from "example/components/Button";
+import { appColors } from "../../consts/colors";
 import inter from "../../assets/inter-medium.ttf";
+import { urlForRoute } from "../../consts/routes";
 
-export default function GettingStartedScreen() {
+export default function GettingStartedScreen(props: { segment: string }) {
   const font = useFont(inter, 12);
   const { state, isActive } = useChartPressState({ x: 0, y: { highTmp: 0 } });
-  const { state: transformState } = useChartTransformState();
+  const url = urlForRoute(props.segment);
+
+  const handleDocsButtonPress = React.useCallback(async () => {
+    url && (await Linking.canOpenURL(url)) && Linking.openURL(url);
+  }, [url]);
 
   return (
-    <View style={{ height: 300 }}>
-      <CartesianChart
-        data={DATA}
-        xKey="day"
-        yKeys={["highTmp"]}
-        axisOptions={{
-          font,
-        }}
-        transformConfig={{
-          pan: {
-            enabled: true,
-            dimensions: ["x", "y"],
-          },
-        }}
-        viewport={{
-          x: [15, 30],
-          y: [15, 120],
-        }}
-        chartPressState={state}
-        transformState={transformState}
-      >
-        {({ points }) => (
-          <>
-            <Line points={points.highTmp} color="red" strokeWidth={3} />
-            {isActive && (
-              <ToolTip x={state.x.position} y={state.y.highTmp.position} />
-            )}
-          </>
-        )}
-      </CartesianChart>
-    </View>
+    <SafeAreaView style={styles.safeView}>
+      <View style={{ margin: 20 }}>
+        <Button onPress={handleDocsButtonPress} title="Read Docs" />
+      </View>
+      <View style={{ flex: 1, maxHeight: 400, padding: 32 }}>
+        <CartesianChart
+          data={DATA}
+          xKey="day"
+          yKeys={["highTmp"]}
+          axisOptions={{
+            font,
+          }}
+          chartPressState={state}
+        >
+          {({ points }) => (
+            <>
+              <Line points={points.highTmp} color="red" strokeWidth={3} />
+              {isActive && (
+                <ToolTip x={state.x.position} y={state.y.highTmp.position} />
+              )}
+            </>
+          )}
+        </CartesianChart>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -58,3 +54,13 @@ const DATA = Array.from({ length: 31 }, (_, i) => ({
   day: i,
   highTmp: 40 + 30 * Math.random(),
 }));
+
+const styles = StyleSheet.create({
+  safeView: {
+    flex: 1,
+    backgroundColor: appColors.viewBackground.light,
+    $dark: {
+      backgroundColor: appColors.viewBackground.dark,
+    },
+  },
+});
