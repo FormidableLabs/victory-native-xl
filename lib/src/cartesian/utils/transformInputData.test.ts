fd231ef5c779fe0017646ebe1a464f282bab8ea6
Eli Zibin <1131641+zibs@users.noreply.github.com>
2026-05-14 12:59:27 -0700

    test(cartesian): cover transform input edge cases

--- lib/src/cartesian/utils/transformInputData.test.ts
+++ lib/src/cartesian/utils/transformInputData.test.ts
@@ -130,5 +130,78 @@ describe("transformInputData", () => {
     expect(y.y.i).toEqual([7, 5, 3]);
   });
 
+  it("preserves input order for ordinal x values", () => {
+    const { ix, ox, xTicksNormalized, isNumericalData } = transformInputData({
+      data: [
+        { x: "beta", y: 3 },
+        { x: "alpha", y: 7 },
+        { x: "gamma", y: 5 },
+      ],
+      xKey: "x",
+      yKeys: ["y"],
+      outputWindow: OUTPUT_WINDOW,
+      xAxis: axes.xAxis,
+      yAxes: axes.yAxes.map((axis) => ({ ...axis, yKeys: ["y"] })),
+    });
+
+    expect(isNumericalData).toBe(false);
+    expect(ix).toEqual(["beta", "alpha", "gamma"]);
+    expect(ox).toEqual([0, 250, 500]);
+    expect(xTicksNormalized).toEqual([0, 1, 2]);
+  });
+
+  it("centers a single datum by expanding equal x and y domains", () => {
+    const { ox, y } = transformInputData({
+      data: [{ x: 5, y: 10 }],
+      xKey: "x",
+      yKeys: ["y"],
+      outputWindow: OUTPUT_WINDOW,
+      xAxis: axes.xAxis,
+      yAxes: axes.yAxes.map((axis) => ({ ...axis, yKeys: ["y"] })),
+    });
+
+    expect(ox).toEqual([250]);
+    expect(y.y.o[0]).toBeCloseTo(150);
+  });
+
+  it("keeps missing y values in the transformed output without using them for scale bounds", () => {
+    const { y, yAxes } = transformInputData({
+      data: [
+        { x: 0, y: 3 },
+        { x: 1, y: null },
+        { x: 2, y: 7 },
+      ],
+      xKey: "x",
+      yKeys: ["y"],
+      outputWindow: OUTPUT_WINDOW,
+      xAxis: axes.xAxis,
+      yAxes: axes.yAxes.map((axis) => ({ ...axis, yKeys: ["y"] })),
+    });
+
+    expect(y.y.i).toEqual([3, null, 7]);
+    expect(y.y.o[1]).toBeNull();
+    expect(yAxes[0].yScale.domain()).toEqual([7, 3]);
+  });
+
+  it("builds separate y scales for multiple y-axis configurations", () => {
+    const baseYAxis = axes.yAxes[0]!;
+    const { yAxes, y } = transformInputData({
+      data: DATA,
+      xKey: "x",
+      yKeys: ["y", "z"],
+      outputWindow: OUTPUT_WINDOW,
+      xAxis: axes.xAxis,
+      yAxes: [
+        { ...baseYAxis, yKeys: ["y"], domain: [0, 10] },
+        { ...baseYAxis, yKeys: ["z"], domain: [0, 100] },
+      ],
+    });
+
+    expect(yAxes[0].yScale(10)).toBeCloseTo(0);
+    expect(yAxes[1]!.yScale(100)).toBeCloseTo(0);
+    expect(y.y.o).toEqual([210, 90.00000000000001, 150]);
+    expect(y.z.o).toEqual([300, 288, 270]);
+  });
+
   // TODO: Some day, test the gridOptions code.
 });
